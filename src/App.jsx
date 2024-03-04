69c1be1a4e83aa1323bf41caa5b4940f8ad2963b
Amit Roy <amitroy497@yahoo.com>
2024-03-04 14:46:29 +0530

    form validation with and without Yup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import FormWithYup from './components/FormWithYup';
+// import FormWithoutYup from './components/FormWithoutYup';
+
+const App = () => {
+	return (
+		<>
+			<FormWithYup />
+		</>
+	);
+};
+
+export default App;
